33dd96622c34cdc64d4850e2762536165c4cc1b6
William Yataco <william_ryv_13@hotmail.com>
2023-12-02 20:24:48 -0500

    second version dockerization

---
+++ 295topics-fullstack/295topics-fullstack.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#stage 0: enviro vars
+BC_PROJECT="295devops"
+HOME_PROJECT="bootcamp-devops-2023"
+URL_REPO="https://github.com/WilliamKidefw/bootcamp-devops-2023.git"
+BRANCH_REPO="ejercicio2-dockeriza"
+TAG_APP_BACKEND="295topics-backend"
+TAG_APP_FRONTEND="295topics-frontend"
+
+#stage 1: cloning the repo
+
+if [ -d $BC_PROJECT ]; then
+	cd 295devops
+else
+	mkdir $BC_PROJECT
+	cd $BC_PROJECT
+fi
+
+if [ -d $HOME_PROJECT ]; then
+	cd $HOME_PROJECT
+	git checkout $BRANCH_REPO
+	git pull
+else
+	git clone $URL_REPO $HOME_PROJECT
+	cd $HOME_PROJECT
+	git checkout $BRANCH_REPO
+fi
+
+#stage 2: Build and Push BackEnd 295fullstack
+cd 295topics-fullstack
+
+docker build -t $TAG_APP_BACKEND -f DockerFile.backend .
+sleep 5
+
+docker build -t $TAG_APP_FRONTEND -f DockerFile.frontend .
+sleep 5
+
+docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
+sleep 5
+
+docker tag $TAG_APP_BACKEND wyataco/$TAG_APP_BACKEND
+sleep 2
+docker push wyataco/$TAG_APP_BACKEND
+sleep 5
+
+docker tag $TAG_APP_FRONTEND wyataco/$TAG_APP_FRONTEND
+sleep 2
+docker push wyataco/$TAG_APP_FRONTEND
+sleep 5
+
+echo "DEPLOYING CONTAINERS"
+sleep 2
+docker-compose -f docker-compose-295topics.yml -p 295topics up -d
+sleep 5
+echo "THE APPLICATION IS UP AND RUNNING"
+sleep 1
+
+echo "BUILD AND PUSH script finished"
+docker logout
+sleep 2
+
+echo "THE APPLICATION IS DESTROYING"
+docker-compose -f docker-compose-295topics.yml -p 295topics down
+docker rmi $TAG_APP_FRONTEND
+docker rmi $TAG_APP_BACKEND
+sleep 5
+
+echo "FINISHED"
